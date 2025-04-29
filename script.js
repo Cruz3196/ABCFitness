6b8117be81cc76de73703b566883880fdd59c93a
test <test>
2025-04-28 17:49:55 -0700

    shopping cart page added

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 function submit_function(){
     alert("Thank you for subscribing")
 }
+
+// function for adding products to the cart 
+function add_to_cart(){
+    alert("Product added to cart")
+}
