07b69c9795b3f8d0d284089eb83383c3a245f3a4
test <test>
2025-04-30 20:05:06 -0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // for the submit section 
-function submit_function(){
+function subscription(){
     alert("Thank you for subscribing")
 }
 
 // function for adding products to the cart 
 function add_to_cart(){
     alert("Product added to cart")
+}
+
+// product removed from function
+function remove_from_cart(){
+    alert("Product removed from cart")
+}
+
+// process order 
+function checkout(){
+    alert("Your order has been processed")
+}
+
+// contact function
+function contact(){
+    alert("Your message has been sent")
 }
